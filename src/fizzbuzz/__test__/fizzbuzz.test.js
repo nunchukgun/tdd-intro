b0fcb1b1478ce766a18a21e499a48828dd9043c8
John McKeever <jmmckeever823@gmail.com>
2019-12-11 15:32:12 -0600

    added stubs for fizzbuzz

---
+++ src/fizzbuzz/__test__/fizzbuzz.test.js
@@ -0,0 +1,10 @@
+import fizzbuzz from '../fizzbuzz'
+
+describe('fizzbuzz', () => {
+    it('is a sample test provided for your benefit', () => {
+        const a = 1
+        const b = 2
+
+        expect(a).toBe(b)
+    })
+})
