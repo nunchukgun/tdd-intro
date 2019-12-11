556dfaa6c0fa7f67e480f77ab1424977c4d6baf2
John McKeever <jmmckeever823@gmail.com>
2019-12-11 16:16:27 -0600

    added instructions to stack exercise file

--- src/stack/stack.js
+++ src/stack/stack.js
@@ -1,3 +1,13 @@
+/*
+Instructions:
+The size method must return the number of elements in the stack.
+The push method must add elements to the end of the stack, to a maximum of ten. Throw if max size exceeded.
+The pop method must remove the last element added to the stack and return it. Throw if no elements in stack.
+The getStack method should return the internal stack variable.
+
+Do not use javascript's native push and pop methods.
+ */
+
 class MyStack {
     constructor(){
         this.stack = []
