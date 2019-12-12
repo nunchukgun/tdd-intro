5c9a5fb1f6850423143d39fb2298dc65e20f52de
John McKeever <jmmckeever823@gmail.com>
2019-12-12 08:55:00 -0600

    added stack stub

---
+++ src/stack/__test__/stack.test.js
@@ -0,0 +1,10 @@
+import MyStack from '../stack'
+
+describe('MyStack', () => {
+    it('is a sample test so that you know how tests work', () => {
+        const a = 1
+        const b = 9
+
+        expect(a).toEqual(b)
+    })
+})
